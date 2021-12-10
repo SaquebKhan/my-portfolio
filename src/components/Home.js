ff5dac3f93db9105b975091cc988dbe2a72b7835
SaquebKhan <82435053+SaquebKhan@users.noreply.github.com>
2021-12-10 00:54:14 -0500

    Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,19 @@
 import React from "react";
+import image from "../Website-Home-Background.jpg";
 
 export default function Home() {
-    return <h1>Home Page!</h1>
+    return (
+    <main>
+        <img 
+        src={image} 
+        alt="Home Background" 
+        className="absolute object-cover w-full h-full"
+        />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-blue-100 font-bold cursive leading-none lg:leading-snug home-name">
+                    Hello, I'm Saqueb Khan. Welcome To My Website
+                </h1>
+            </section>
+        </main>
+    );
 }
